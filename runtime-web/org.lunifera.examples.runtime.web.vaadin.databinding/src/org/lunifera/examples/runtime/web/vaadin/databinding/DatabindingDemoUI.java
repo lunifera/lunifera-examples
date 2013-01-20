ce5c61124a0e444f0a34f36c728a887f2a4f54fa
Florian Pirchner <florian.pirchner@gmail.com>
2013-01-20 22:22:59 +0100

    Added an example

--- runtime-web/org.lunifera.examples.runtime.web.vaadin.databinding/src/org/lunifera/examples/runtime/web/vaadin/databinding/DatabindingDemoUI.java
+++ runtime-web/org.lunifera.examples.runtime.web.vaadin.databinding/src/org/lunifera/examples/runtime/web/vaadin/databinding/DatabindingDemoUI.java
@@ -13,6 +13,8 @@
 package org.lunifera.examples.runtime.web.vaadin.databinding;
 
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.conversion.IConverter;
 import org.lunifera.runtime.web.vaadin.databinding.IVaadinObservableValue;
 import org.lunifera.runtime.web.vaadin.databinding.VaadinObservables;
 import org.lunifera.web.vaadin.common.OSGiUI;
@@ -22,6 +24,7 @@ import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Slider;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.themes.Reindeer;
 
@@ -41,7 +44,7 @@ public class DatabindingDemoUI extends OSGiUI {
 		VaadinObservables.getRealm(getUI());
 		dbc = new DataBindingContext();
 
-		GridLayout layout = new GridLayout(4, 3);
+		GridLayout layout = new GridLayout(3, 4);
 		layout.setStyleName(Reindeer.LAYOUT_BLUE);
 		layout.setWidth("100%");
 		layout.setMargin(true);
@@ -50,7 +53,7 @@ public class DatabindingDemoUI extends OSGiUI {
 		row1(layout);
 		row2(layout);
 		row3(layout);
-
+		row4(layout);
 	}
 
 	private void row1(GridLayout layout) {
@@ -89,12 +92,8 @@ public class DatabindingDemoUI extends OSGiUI {
 		layout.addComponent(readonly, 0, 1);
 		layout.addComponent(textField, 1, 1);
 
-		IVaadinObservableValue checkboxObservable = VaadinObservables
-				.observeValue(readonly);
-		IVaadinObservableValue textFieldObservable = VaadinObservables
-				.observeReadonly(textField);
-
-		dbc.bindValue(textFieldObservable, checkboxObservable);
+		dbc.bindValue(VaadinObservables.observeReadonly(textField),
+				VaadinObservables.observeValue(readonly));
 	}
 
 	private void row3(GridLayout layout) {
@@ -109,6 +108,7 @@ public class DatabindingDemoUI extends OSGiUI {
 		TextField input = new TextField("input");
 		input.setImmediate(true);
 		input.setBuffered(false);
+		input.setValidationVisible(true);
 
 		layout.addComponent(required, 0, 2);
 		layout.addComponent(requiredError, 1, 2);
@@ -130,4 +130,50 @@ public class DatabindingDemoUI extends OSGiUI {
 		dbc.bindValue(value_requiredObservable, readonly_errorString);
 		dbc.bindValue(value_errorString, errorMessage_Input);
 	}
+
+	private void row4(GridLayout layout) {
+		Slider slider = new Slider("width");
+		slider.setImmediate(true);
+		slider.setBuffered(false);
+		slider.setWidth("150px");
+		layout.addComponent(slider, 0, 3);
+
+		TextField heightInput = new TextField("height");
+		heightInput.setImmediate(true);
+		heightInput.setBuffered(false);
+		layout.addComponent(heightInput, 1, 3);
+
+		slider.setMax(200);
+		slider.setValue(20d);
+		heightInput.setValue("10px");
+
+		TextField input2 = new TextField("sizeable");
+		layout.addComponent(input2, 2, 3);
+
+		dbc.bindValue(VaadinObservables.observeWidth(input2), VaadinObservables
+				.observeValue(slider), null, new UpdateValueStrategy(
+				UpdateValueStrategy.POLICY_UPDATE)
+				.setConverter(new IConverter() {
+					@Override
+					public Object getToType() {
+						return String.class;
+					}
+
+					@Override
+					public Object getFromType() {
+						return Object.class;
+					}
+
+					@Override
+					public Object convert(Object fromObject) {
+						double value = (Double) fromObject;
+						return String.format("%s%s", Double.toString(value),
+								Unit.PIXELS.getSymbol());
+					}
+				}));
+		dbc.bindValue(VaadinObservables.observeHeight(input2),
+				VaadinObservables.observeValue(heightInput));
+
+	}
+
 }
